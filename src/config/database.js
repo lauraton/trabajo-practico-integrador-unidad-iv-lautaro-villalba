b3d07c1afe8d337f0b33005ecf4fe92851cf52e8
lauraton <nickk5926@gmail.com>
2026-07-01 17:47:28 -0300

    Feat: se agregan las validaciones dentro de los controllers

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,17 @@
-import { hostname } from "node:os";
+
 import { Sequelize } from "sequelize";
 
-export const sequelize = new sequelize('movies','root','',{
+export const sequelize = new Sequelize('movies','root','',{
     host:"localhost",
     dialect:"mysql",
 });
 
 export const startDB = async () => {
-    try{
+    try {
         await sequelize.authenticate();
-        await sequelize.sync({force:false});
-        console.log('Conexion a la db lista');
+        await sequelize.sync({ force: false });
+        console.log('Conexión a la bd lista');
     } catch (error) {
-        console.error('No se pudo conectar a la db',error);
+        console.error('No se pudo conectar a la db:', error);
     }
 }
